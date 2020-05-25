53c2c801b347e6ac734cb59e13e95904a2ed5674
evowilliamson <14161480+evowilliamson@users.noreply.github.com>
2020-05-25 09:04:18 +0200

    Add refresh script for python-viz-algos

---
+++ refresh_python_algos.sh
@@ -0,0 +1,16 @@
+rm -Rf /tmp/python-algos_
+cd /home/ivo/projects/python-algorithms
+python setup.py sdist
+cd dist
+cp python-algos* /tmp
+cd /tmp
+mv python-algos* python-algos.tar.gz
+gunzip python-algos.tar.gz
+tar -xvf python-algos.tar
+DIR=`ls -als | grep python-algos | grep drw | awk '{print $NF}'`
+mv $DIR python-algos_
+cd python-algos_
+rm -Rf /home/ivo/projects/python-viz-algos/venv/lib/python3.7/site-packages/pythonalgos
+cp -R pythonalgos /home/ivo/projects/python-viz-algos/venv/lib/python3.7/site-packages/pythonalgos
+
+
