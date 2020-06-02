04cf3db9cf128294829207d60693fc4edef54b6d
evowilliamson <14161480+evowilliamson@users.noreply.github.com>
2020-06-02 20:35:45 +0200

    Adding more and change some

--- refresh_python_algos.sh
+++ refresh_python_algos.sh
@@ -10,7 +10,7 @@ tar -xvf python-algos.tar
 DIR=`ls -als | grep python-algos | grep drw | awk '{print $NF}'`
 mv $DIR python-algos_
 cd python-algos_
-rm -Rf /home/ivo/projects/python-viz-algos/venv/lib/python3.7/site-packages/pythonalgos
-cp -R pythonalgos /home/ivo/projects/python-viz-algos/venv/lib/python3.7/site-packages/pythonalgos
+rm -Rf /home/ivo/projects/python-viz-algos/venv/lib/python3.8/site-packages/pythonalgos
+cp -R pythonalgos /home/ivo/projects/python-viz-algos/venv/lib/python3.8/site-packages/pythonalgos
 
 
